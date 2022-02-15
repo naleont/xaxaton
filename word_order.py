57e67619c91fe83a0312891fa5cdd1e42cb1b46d
katieTHEbest <56514599+katieTHEbest@users.noreply.github.com>
2022-02-15 11:55:43 +0300

    Add files via upload

---
+++ word_order.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def quotes_order(dataset):
+    df = pd.read_csv(dataset)
+    quotes = df['Text'].values
+    w_orders = []
+    for sent in quotes:
+        w_orders.append(word_order(sent))
+    return w_orders
+
+def word_order(sent):
+    d = nlp(sent)
+    roots = [token for token in d if token.head == token]
+    orders = []
+    for root in roots:
+        d = {}
+        d['V'] = root.i
+        for child in root.children:
+            if child.dep_ == 'nsubj':
+                d['S'] = child.i
+            if child.dep_== 'dobj':
+                d['O'] = child.i
+        listt = sorted(d.keys(),key=d.get)
+        order = ''.join(listt)
+        orders.append(order)
+    return orders
+
